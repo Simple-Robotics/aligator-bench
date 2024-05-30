2b6493756f8a98d1711e4e7e1d23079962629222
ManifoldFR <wilson.jallet@polytechnique.org>
2024-05-30 19:51:47 +0200

    stump ur5 file

--- benchmarks/ur5.cpp
+++ benchmarks/ur5.cpp
@@ -1,2 +1,64 @@
+#include "aligator/core/cost-abstract.hpp"
+#include "aligator/core/traj-opt-problem.hpp"
 
-int main() { return 0; }
+#include "aligator/modelling/costs/quad-state-cost.hpp"
+#include "aligator/modelling/costs/sum-of-costs.hpp"
+#include "aligator/modelling/dynamics/context.hpp"
+#include "aligator/modelling/dynamics/integrator-semi-euler.hpp"
+#include "aligator/modelling/dynamics/multibody-free-fwd.hpp"
+
+#include "robots/robot_spec.hpp"
+#include <pinocchio/parsers/urdf.hpp>
+
+namespace alcontext = aligator::context;
+namespace pin = pinocchio;
+using alcontext::CostAbstract;
+using alcontext::StageModel;
+using alcontext::TrajOptProblem;
+using alcontext::VectorXs;
+using xyz::polymorphic;
+
+using aligator_bench::loadRobotSpecFromToml;
+using aligator_bench::PACKAGE_DIRS_BASE;
+using aligator_bench::robot_spec;
+
+using MultibodyFreeFwd =
+    aligator::dynamics::MultibodyFreeFwdDynamicsTpl<double>;
+using Space = proxsuite::nlp::MultibodyPhaseSpace<double>;
+using aligator::dynamics::IntegratorSemiImplEulerTpl;
+
+auto createCost(Space space) {
+  aligator::CostStackTpl<double> costs{space, space.getModel().nv};
+  return costs;
+}
+
+auto createProblem() {
+  const double dt = 1e-2;
+  const size_t nsteps = 100;
+
+  robot_spec spec = loadRobotSpecFromToml("ur.toml", "ur5");
+
+  pin::Model model;
+  pin::GeometryModel collModel;
+  pin::urdf::buildModel(spec.urdfPath, model, true);
+  pin::urdf::buildGeom(model, spec.urdfPath, pin::GeometryType::COLLISION,
+                       collModel, PACKAGE_DIRS_BASE);
+
+  Space state_space{model};
+  const VectorXs x0 = state_space.neutral();
+
+  const MultibodyFreeFwd ode{state_space};
+  const IntegratorSemiImplEulerTpl<double> dynamics{ode, dt};
+  const auto rcost = createCost(state_space);
+  auto tcost = rcost;
+
+  StageModel stage{rcost, dynamics};
+
+  std::vector<polymorphic<StageModel>> stages{nsteps, stage};
+  return TrajOptProblem{x0, stages, tcost};
+}
+
+int main() {
+  auto problem = createProblem();
+  return 0;
+}
