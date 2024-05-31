365537b4bfd0d240e39b66e79d96491e03d9ecd4
ManifoldFR <wilson.jallet@polytechnique.org>
2024-05-31 23:25:17 +0200

    basic ur5 prob

--- benchmarks/ur5.cpp
+++ benchmarks/ur5.cpp
@@ -1,41 +1,62 @@
-#include "aligator/core/cost-abstract.hpp"
-#include "aligator/core/traj-opt-problem.hpp"
+#include <aligator/core/traj-opt-problem.hpp>
 
-#include "aligator/modelling/costs/quad-state-cost.hpp"
-#include "aligator/modelling/costs/sum-of-costs.hpp"
-#include "aligator/modelling/dynamics/context.hpp"
-#include "aligator/modelling/dynamics/integrator-semi-euler.hpp"
-#include "aligator/modelling/dynamics/multibody-free-fwd.hpp"
+#include <aligator/modelling/costs/quad-costs.hpp>
+#include <aligator/modelling/costs/quad-state-cost.hpp>
+#include <aligator/modelling/costs/sum-of-costs.hpp>
+#include <aligator/modelling/dynamics/integrator-semi-euler.hpp>
+#include <aligator/modelling/dynamics/multibody-free-fwd.hpp>
+
+#include <aligator/solvers/proxddp/solver-proxddp.hpp>
 
 #include "robots/robot_load.hpp"
 
 namespace alcontext = aligator::context;
 namespace pin = pinocchio;
 using alcontext::CostAbstract;
+using alcontext::MatrixXs;
 using alcontext::StageModel;
 using alcontext::TrajOptProblem;
 using alcontext::VectorXs;
 using xyz::polymorphic;
-
-using aligator_bench::loadRobotSpecFromToml;
-using aligator_bench::PACKAGE_DIRS_BASE;
-using aligator_bench::robot_spec;
 
 using MultibodyFreeFwd =
     aligator::dynamics::MultibodyFreeFwdDynamicsTpl<double>;
 using Space = proxsuite::nlp::MultibodyPhaseSpace<double>;
 using aligator::dynamics::IntegratorSemiImplEulerTpl;
 
-auto createCost(Space space) {
-  aligator::CostStackTpl<double> costs{space, space.getModel().nv};
+auto createCost(Space space, double dt) {
+  auto &model = space.getModel();
+  const auto nu = model.nv;
+  const auto ndx = space.ndx();
+  aligator::CostStackTpl<double> costs{space, nu};
+
+  MatrixXs Wx{ndx, ndx};
+  Wx.setIdentity();
+
+  MatrixXs Wu{nu, nu};
+  Wu.setIdentity();
+  aligator::QuadraticCostTpl<double> quadcost{Wx, Wu};
+
+  costs.addCost(quadcost, 1e-4 * dt);
+
   return costs;
 }
 
-auto createProblem() {
-  const double dt = 1e-2;
-  const size_t nsteps = 100;
+auto createTerminalCost(Space space, VectorXs xf) {
+  const auto nu = space.getModel().nv;
+  MatrixXs weights{space.ndx(), space.ndx()};
+  weights.setIdentity();
+  aligator::QuadraticStateCostTpl<double> quadf{space, nu, xf, weights};
+  return quadf;
+}
 
-  pin::Model model = aligator_bench::loadModelFromToml("ur.toml", "ur5");
+TrajOptProblem createProblem() {
+  const double tf = 1.0;
+  const double dt = 1e-2;
+  const size_t nsteps = size_t(tf / dt);
+
+  pin::Model model;
+  aligator_bench::loadModelFromToml("ur.toml", "ur5", model);
   std::cout << model << std::endl;
 
   Space state_space{model};
@@ -43,8 +64,10 @@ auto createProblem() {
 
   const MultibodyFreeFwd ode{state_space};
   const IntegratorSemiImplEulerTpl<double> dynamics{ode, dt};
-  const auto rcost = createCost(state_space);
-  auto tcost = rcost;
+  const auto rcost = createCost(state_space, dt);
+
+  VectorXs xf = state_space.rand();
+  const auto tcost = createTerminalCost(state_space, xf);
 
   StageModel stage{rcost, dynamics};
 
@@ -54,5 +77,14 @@ auto createProblem() {
 
 int main() {
   auto problem = createProblem();
+
+  const double mu_init = 1e-3;
+  alcontext::SolverProxDDP solver{1e-4, mu_init};
+  solver.verbose_ = aligator::VERBOSE;
+  solver.linear_solver_choice = aligator::LQSolverChoice::SERIAL;
+
+  solver.setup(problem);
+  solver.run(problem);
+
   return 0;
 }
