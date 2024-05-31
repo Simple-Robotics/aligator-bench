8d701f2cd7fe7351e179eed6ace1eb441c8f61be
ManifoldFR <wilson.jallet@polytechnique.org>
2024-05-31 23:25:47 +0200

    [robots] remove inlined Model-returning function, change include to pinocchio/multibody/fwd

--- src/robots/robot_load.hpp
+++ src/robots/robot_load.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <pinocchio/multibody/model.hpp>
+#include <pinocchio/multibody/fwd.hpp>
 
 namespace aligator_bench {
 struct robot_spec;
@@ -9,12 +9,5 @@ void loadModelFromSpec(const robot_spec &spec, pinocchio::Model &model);
 
 void loadModelFromToml(const std::string &tomlFile, const std::string &key,
                        pinocchio::Model &model);
-
-inline pinocchio::Model loadModelFromToml(const std::string &tomlFile,
-                                          const std::string &key) {
-  pinocchio::Model model;
-  loadModelFromToml(tomlFile, key, model);
-  return model;
-}
 
 } // namespace aligator_bench
