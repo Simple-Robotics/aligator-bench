1c4d93cf216a4743fb5ff9bc95a9976243cf366a
ManifoldFR <wilson.jallet@polytechnique.org>
2024-09-12 14:53:33 +0200

    Add aligator to altro conversion functions

---
+++ src/aligator-to-altro.cpp
@@ -0,0 +1,67 @@
+#include "aligator-to-altro.hpp"
+
+auto aligatorCostToAltro(xyz::polymorphic<CostAbstract> aliCost) -> altroCostTriplet {
+  auto data = aliCost->createData();
+  
+  using altro::a_float;
+  altro::CostFunction f = [aliCost, data] (const a_float *x_, const a_float *u_) -> a_float {
+    const auto nx = aliCost->nx();
+    const auto nu = aliCost->nu;
+    ConstVecMap x {x_, nx};
+    ConstVecMap u {u_ , nu};
+    aliCost->evaluate(x, u, *data);
+    return data->value_;
+  };
+  altro::CostGradient gf = [aliCost, data] (a_float *dx_, a_float *du_, const a_float *x_, const a_float *u_) {
+    const auto nx = aliCost->nx();
+    const auto nu = aliCost->nu;
+    VecMap dx {dx_, nx};
+    VecMap du {du_, nu};
+    ConstVecMap x {x_, nx};
+    ConstVecMap u {u_ , nu};
+    aliCost->computeGradients(x, u, *data);
+    dx = data->Lx_;
+    du = data->Lu_;
+  };
+  altro::CostHessian Hf = [aliCost, data] (a_float *ddx_, a_float *ddu_, a_float *dxdu_, const a_float *x_, const a_float *u_) {
+    const auto nx = aliCost->nx();
+    const auto nu = aliCost->nu;
+    MatMap ddx {ddx_, nx, nx};
+    MatMap ddu {ddu_, nu, nu};
+    MatMap dxdu {dxdu_, nx, nu};
+    ConstVecMap x {x_, nx};
+    ConstVecMap u {u_ , nu};
+    aliCost->computeHessians(x, u, *data);
+    ddx = data->Lxx_;
+    ddu = data->Luu_;
+    dxdu = data->Lxu_;
+  };
+  return {f, gf, Hf};
+}
+
+auto aligatorExpDynamicsToAltro(xyz::polymorphic<ExplicitDynamics> dynamics) -> altroExplicitDynamics {
+  auto data = std::static_pointer_cast<alcontext::ExplicitDynamicsData>(dynamics->createData());
+
+  altro::ExplicitDynamicsFunction f = [dynamics, data] (double * xnext_, const double *x_, const double *u_, float) {
+    const auto nx1 = dynamics->nx1();
+    const auto nu = dynamics->nu;
+    const auto nx2 = dynamics->nx2();
+    ConstVecMap x{x_, nx1};
+    ConstVecMap u{u_, nu};
+    VecMap xnext{xnext_, nx2};
+    dynamics->forward(x, u, *data);
+    xnext = data->xnext_;
+  };
+  altro::ExplicitDynamicsJacobian Jf = [dynamics, data] (double *J_, const double*x_, const double * u_, float) {
+    const auto nx1 = dynamics->nx1();
+    const auto nu = dynamics->nu;
+    const auto nx2 = dynamics->nx2();
+    MatMap J{J_, nx2, nx1 + nu};
+    ConstVecMap x{x_, nx1};
+    ConstVecMap u{u_, nu};
+    dynamics->dForward(x, u, *data);
+    J.leftCols(nx1) = data->Jx_;
+    J.rightCols(nu) = data->Ju_;
+  };
+  return std::tuple{f, Jf};
+}
---
+++ src/aligator-to-altro.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <aligator/core/cost-abstract.hpp>
+#include <aligator/core/explicit-dynamics.hpp>
+#include <altro/solver/typedefs.hpp>
+
+namespace alcontext = aligator::context;
+using alcontext::CostAbstract;
+using alcontext::ExplicitDynamics;
+using xyz::polymorphic;
+
+/* Typedefs */
+
+using ConstVecMap = Eigen::Map<const alcontext::VectorXs>;
+using VecMap = Eigen::Map<alcontext::VectorXs>;
+using ConstMatMap = Eigen::Map<const alcontext::MatrixXs>;
+using MatMap = Eigen::Map<alcontext::MatrixXs>;
+
+using altroCostTriplet = std::tuple<altro::CostFunction, altro::CostGradient, altro::CostHessian>;
+using altroExplicitDynamics = std::tuple<altro::ExplicitDynamicsFunction, altro::ExplicitDynamicsJacobian>;
+
+/// @brief Convert aligator cost function to altro
+auto aligatorCostToAltro(xyz::polymorphic<CostAbstract> aliCost) -> altroCostTriplet;
+
+auto aligatorExpDynamicsToAltro(xyz::polymorphic<ExplicitDynamics> dynamics) -> altroExplicitDynamics;
