7836bbbf2ff884d167bcf162566390efd0ffae5f
ManifoldFR <wilson.jallet@polytechnique.org>
2024-10-03 18:40:11 +0200

    handle constraints from aligator problems to Altro

--- src/aligator-problem-to-altro.cpp
+++ src/aligator-problem-to-altro.cpp
@@ -8,37 +8,50 @@
 namespace aligator_bench {
 altro::ALTROSolver *
 initAltroFromAligatorProblem(const alcontext::TrajOptProblem &problem) {
-  auto horizon_length = (int)problem.numSteps();
-  auto x0 = problem.getInitState();
+  const auto horizon_length = (int)problem.numSteps();
+  const auto x0 = problem.getInitState();
   auto *psolver = new altro::ALTROSolver{horizon_length};
   altro::ALTROSolver &solver = *psolver;
   solver.SetInitialState(x0.data(), int(x0.size()));
 
+  auto constraint_handle = [&solver](int nx, int k,
+                                     const alcontext::ConstraintStack &cstrs) {
+    // constraints
+    if (cstrs.size() == 1) {
+      auto [c, Jc, ct] =
+          aligatorConstraintToAltro(nx, {cstrs.funcs[0], cstrs.sets[0]});
+      const int dim = (int)cstrs.dims()[0];
+      solver.SetConstraint(c, Jc, dim, ct, "constraint", k, k + 1);
+    } else if (cstrs.size() > 1) {
+      throw std::runtime_error("Multiple constraints not supported.");
+    }
+  };
+
   solver.SetTimeStep(1.0); // Aligator already handles the timestep
   for (int k = 0; k < horizon_length; k++) {
-    fmt::println("Adding stage {:d}", k);
-    auto &stage = problem.stages_[size_t(k)];
+    const auto &stage = problem.stages_[size_t(k)];
+    const int nx = stage->nx1();
     solver.SetDimension(stage->nx1(), stage->nu(), k, k + 1);
-    fmt::println("> Set dimension");
 
     // cost
     auto [c, gc, Hc] = aligatorCostToAltro(stage->cost_);
     solver.SetCostFunction(c, gc, Hc, k, k + 1);
-    fmt::println("> Set cost");
 
     // dynamics
     auto [dyn, Jdyn] = aligatorExpDynamicsToAltro(stage->dynamics_);
     solver.SetExplicitDynamics(dyn, Jdyn, k, k + 1);
-    fmt::println("> Set dynamics");
+
+    // constraints
+    constraint_handle(nx, k, stage->constraints_);
   }
 
   {
     // terminal
-    int nxterm = problem.term_cost_->nx();
+    const int nxterm = problem.term_cost_->nx();
     solver.SetDimension(nxterm, problem.term_cost_->nu, horizon_length);
     auto [tc, gtc, Htc] = aligatorCostToAltro(problem.term_cost_);
     solver.SetCostFunction(tc, gtc, Htc, horizon_length);
-    fmt::println("> Set terminal cost");
+    constraint_handle(nxterm, horizon_length, problem.term_cstrs_);
   }
 
   return psolver;
