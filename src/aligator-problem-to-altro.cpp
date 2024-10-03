eac3cf19024243a8ad36de2b14cf75772541bcb3
ManifoldFR <wilson.jallet@polytechnique.org>
2024-10-03 16:28:47 +0200

    [python] expose getters, options, for ALTROSolver

--- src/aligator-problem-to-altro.cpp
+++ src/aligator-problem-to-altro.cpp
@@ -2,6 +2,7 @@
 #include "aligator-to-altro-types.hpp"
 
 #include <aligator/core/traj-opt-problem.hpp>
+#include <aligator/core/cost-abstract.hpp>
 #include <aligator/core/explicit-dynamics.hpp>
 
 namespace aligator_bench {
@@ -15,22 +16,29 @@ initAltroFromAligatorProblem(const alcontext::TrajOptProblem &problem) {
 
   solver.SetTimeStep(1.0); // Aligator already handles the timestep
   for (int k = 0; k < horizon_length; k++) {
+    fmt::println("Adding stage {:d}", k);
     auto &stage = problem.stages_[size_t(k)];
-    solver.SetDimension(stage->nx1(), stage->nu(), k, k);
+    solver.SetDimension(stage->nx1(), stage->nu(), k, k + 1);
+    fmt::println("> Set dimension");
 
     // cost
     auto [c, gc, Hc] = aligatorCostToAltro(stage->cost_);
-    solver.SetCostFunction(c, gc, Hc, k, k);
+    solver.SetCostFunction(c, gc, Hc, k, k + 1);
+    fmt::println("> Set cost");
 
     // dynamics
     auto [dyn, Jdyn] = aligatorExpDynamicsToAltro(stage->dynamics_);
-    solver.SetExplicitDynamics(dyn, Jdyn, k, k);
+    solver.SetExplicitDynamics(dyn, Jdyn, k, k + 1);
+    fmt::println("> Set dynamics");
   }
 
   {
     // terminal
+    int nxterm = problem.term_cost_->nx();
+    solver.SetDimension(nxterm, problem.term_cost_->nu, horizon_length);
     auto [tc, gtc, Htc] = aligatorCostToAltro(problem.term_cost_);
     solver.SetCostFunction(tc, gtc, Htc, horizon_length);
+    fmt::println("> Set terminal cost");
   }
 
   return psolver;
--- src/python/expose-altro.cpp
+++ src/python/expose-altro.cpp
@@ -2,6 +2,36 @@
 
 #include <altro/altro.hpp>
 
+Eigen::VectorXd get_state_wrap(const altro::ALTROSolver &solver, int k) {
+  Eigen::VectorXd x{solver.GetStateDim(k)};
+  solver.GetState(x.data(), k);
+  return x;
+}
+
+Eigen::VectorXd get_input_wrap(const altro::ALTROSolver &solver, int k) {
+  Eigen::VectorXd u{solver.GetInputDim(k)};
+  solver.GetInput(u.data(), k);
+  return u;
+}
+
+auto altro_get_all_states(const altro::ALTROSolver &solver) {
+  const size_t horizon_length = size_t(solver.GetHorizonLength());
+  std::vector<Eigen::VectorXd> xs;
+  for (size_t k = 0; k <= horizon_length; k++) {
+    xs.push_back(get_state_wrap(solver, int(k)));
+  }
+  return xs;
+}
+
+auto altro_get_all_inputs(const altro::ALTROSolver &solver) {
+  const size_t horizon_length = size_t(solver.GetHorizonLength());
+  std::vector<Eigen::VectorXd> us;
+  for (size_t k = 0; k < horizon_length; k++) {
+    us.push_back(get_input_wrap(solver, int(k)));
+  }
+  return us;
+}
+
 void exposeAltro() {
   using namespace altro;
 
@@ -10,7 +40,18 @@ void exposeAltro() {
   bp::class_<ALTROSolver, boost::noncopyable>(
       "ALTROSolver", "A wrapper class for the ALTRO solver", bp::no_init)
       // .def(bp::init<int>("self"_a)) // we should never init from Python
+      .def(
+          "GetOptions",
+          +[](ALTROSolver &self) -> AltroOptions & {
+            return self.GetOptions();
+          },
+          ("self"_a), bp::return_internal_reference<>())
       .def("SetOptions", &ALTROSolver::SetOptions, ("self"_a, "opts"))
+      .def("CalcCost", &ALTROSolver::CalcCost, ("self"_a))
+      .def("GetIterations", &ALTROSolver::GetIterations, ("self"_a))
+      .def("GetPrimalFeasibility", &ALTROSolver::GetPrimalFeasibility,
+           ("self"_a))
+      .def("GetFinalObjective", &ALTROSolver::GetFinalObjective, ("self"_a))
       .def(
           "SetInitialState",
           +[](ALTROSolver &self, Eigen::Map<Eigen::VectorXd> x0) {
@@ -18,24 +59,28 @@ void exposeAltro() {
           })
       .def("Initialize", &ALTROSolver::Initialize, ("self"_a))
       .def("Solve", &ALTROSolver::Solve, ("self"_a))
-      .def(
-          "GetState",
-          +[](const ALTROSolver &self, int k) {
-            Eigen::VectorXd x{self.GetStateDim(k)};
-            self.GetState(x.data(), k);
-            return x;
-          })
-      .def(
-          "GetInput",
-          +[](const ALTROSolver &self, int k) {
-            Eigen::VectorXd u{self.GetInputDim(k)};
-            self.GetInput(u.data(), k);
-            return u;
-          })
+      .def("GetState", &get_state_wrap, ("self"_a, "k"))
+      .def("GetInput", &get_input_wrap, ("self"_a, "k"))
       .def("PrintStateTrajectory", &ALTROSolver::PrintStateTrajectory,
            ("self"_a))
       .def("PrintInputTrajectory", &ALTROSolver::PrintInputTrajectory,
            ("self"_a));
+
+  bp::def("altroGetAllStates", &altro_get_all_states, ("solver"_a),
+          "Get all states from the solver.");
+  bp::def("altroGetAllInputs", &altro_get_all_inputs, ("solver"_a),
+          "Get all inputs from the solver.");
+
+  bp::enum_<SolveStatus>("SolveStatus")
+#define _c(name) value(#name, SolveStatus::name)
+      ._c(Success)
+      ._c(Unsolved)
+      ._c(MaxIterations)
+      ._c(MaxObjectiveExceeded)
+      ._c(StateOutOfBounds)
+      ._c(InputOutOfBounds)
+      ._c(MeritFunGradientTooSmall);
+#undef _c
 
   bp::enum_<Verbosity>("AltroVerbosity")
 #define _c(name) value(#name, Verbosity::name)
@@ -80,12 +125,15 @@ void exposeAltro() {
       ._c(iterations_max)
       ._c(tol_cost)
       ._c(tol_cost_intermediate)
+      ._c(tol_primal_feasibility)
       ._c(tol_stationarity)
       ._c(tol_meritfun_gradient)
       ._c(max_state_value)
       ._c(max_input_value)
       ._c(penalty_initial)
       ._c(penalty_scaling)
-      ._c(penalty_max);
+      ._c(penalty_max)
+      ._c(verbose)
+      ._c(use_backtracking_linesearch);
 #undef _c
 }
