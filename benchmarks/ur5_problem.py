dd541a4d8d4d8096812e05f1c7d5434c696b3f29
ManifoldFR <wilson.jallet@polytechnique.org>
2024-10-15 11:49:51 +0200

    ur5_problem : use box constraint

--- benchmarks/ur5_problem.py
+++ benchmarks/ur5_problem.py
@@ -7,44 +7,44 @@ import matplotlib.pyplot as plt
 from aligator import constraints, manifolds
 
 
+def get_default_config_ee_pose(rmodel: pin.Model, ee_name: str):
+    q0 = pin.neutral(rmodel)
+    rdata = rmodel.createData()
+    ee_id = rmodel.getFrameId(ee_name)
+    pin.framesForwardKinematics(rmodel, rdata, q0)
+    M_tool_q0: pin.SE3 = rdata.oMf[ee_id]
+    return M_tool_q0.translation.copy()
+
+
 class URProblem(object):
     robot = erd.load("ur5")
     EE_NAME = "tool0"
 
     def __init__(self, vel_constraint=False, ee_target=None):
         rmodel: pin.Model = self.robot.model.copy()
+        q0 = pin.neutral(rmodel)
 
         if ee_target is None:
-            ee_target = 0.98 * self.get_default_config_ee_pose(rmodel)
+            ee_target = 0.7 * get_default_config_ee_pose(rmodel, self.EE_NAME)
 
         self.problem, self.times = self._build_problem(
-            rmodel, vel_constraint, ee_target, self.EE_NAME
+            rmodel, q0, vel_constraint, ee_target, self.EE_NAME
         )
         self._vel_constraint = vel_constraint
         self.rmodel = rmodel
-
-    @staticmethod
-    def get_default_config_ee_pose(rmodel: pin.Model):
-        q0 = pin.neutral(rmodel)
-        rdata = rmodel.createData()
-        ee_id = rmodel.getFrameId(URProblem.EE_NAME)
-        pin.framesForwardKinematics(rmodel, rdata, q0)
-        M_tool_q0: pin.SE3 = rdata.oMf[ee_id]
-        return M_tool_q0.translation.copy()
 
     def name(self):
         return "UR5 Reach"
 
     @staticmethod
-    def _build_problem(rmodel: pin.Model, vel_constraint, ee_target, ee_name: int):
-
+    def _build_problem(rmodel: pin.Model, q0, vel_constraint, ee_target, ee_name: int):
         nv = rmodel.nv
         nu = nv
 
         space = manifolds.MultibodyPhaseSpace(rmodel)
         ndx = space.ndx
 
-        x0 = space.neutral()
+        x0 = np.concatenate((q0, np.zeros(nv)))
         xneut = space.neutral()
         dt = 0.01
 
@@ -78,58 +78,82 @@ class URProblem(object):
             if vel_constraint:
                 vmax = rmodel.velocityLimit
                 vmin = -vmax
-                _b = np.concatenate([-vmax, vmin])
-                _A = np.zeros((2 * nv, ndx))
-                _A[:nv, nv:] = +np.eye(nv)
-                _A[nv:, nv:] = -np.eye(nv)
-                vel_box_fn = aligator.LinearFunction(ndx, nu, ndx, 2 * nv)
-                vel_box_fn.A[:] = _A
-                vel_box_fn.d[:] = _b
-                stage.addConstraint(vel_box_fn, constraints.NegativeOrthant())
+                stage.addConstraint(
+                    aligator.StateErrorResidual(space, nu, xneut)[nv:],
+                    constraints.BoxConstraint(vmin, vmax),
+                )
             problem.addStage(stage)
         return (problem, times)
 
 
 if __name__ == "__main__":
-    from .solver_runner import AltroRunner
+    from tap import Tap
+    from .solver_runner import ProxDdpRunner, AltroRunner
 
-    ur_problem = URProblem()
-    problem = ur_problem.problem
+    class Args(Tap):
+        plot: bool = False
+
+    args = Args().parse_args()
+    ur_problem = URProblem(True)
     rmodel = ur_problem.rmodel
     nq = rmodel.nq
     times_ = ur_problem.times
 
-    TOL = 1e-5
-    mu_init = 1e-1
+    TOL = 1e-3
+    mu_init = 1e1
 
     MAX_ITER = 400
-    alisolver = aligator.SolverProxDDP(TOL, mu_init, verbose=aligator.VERBOSE)
-    alisolver.max_iters = MAX_ITER
-    bcl_params: alisolver.AlmParams = alisolver.bcl_params
-    bcl_params.mu_lower_bound = 1e-10
-    alisolver.setup(problem)
-    alisolver.run(problem)
+    alirunner = ProxDdpRunner(
+        {
+            "rollout_type": aligator.ROLLOUT_LINEAR,
+            "verbose": True,
+            "max_iters": MAX_ITER,
+            "mu_init": mu_init,
+        }
+    )
+    alirunner.solve(ur_problem, TOL)
+    alisolver = alirunner.solver
+
     ali_iter = alisolver.results.num_iters
 
     print(alisolver.results)
 
     xs_ali = alisolver.results.xs.tolist()
     us_ali = alisolver.results.us.tolist()
+    vs_ali = [x[nq:] for x in xs_ali]
 
-    runner = AltroRunner({"verbose": True})
-    altro_res = runner.solve(ur_problem, TOL)
+    altrorunner = AltroRunner(
+        {
+            "verbose": True,
+            "max_iters": MAX_ITER,
+            "mu_init": mu_init,
+            "tol_stationarity": mu_init,
+        }
+    )
+    altro_res = altrorunner.solve(ur_problem, TOL)
     print(altro_res)
-    altro_solve = runner.solver
+    altro_solve = altrorunner.solver
 
     xs_altro = altro_solve.GetAllStates().tolist()
     us_altro = altro_solve.GetAllInputs().tolist()
-
-    vs_ali = [x[nq:] for x in xs_ali]
     vs_altro = [x[nq:] for x in xs_altro]
-    fig1, axes1 = aligator.utils.plotting.plot_controls_traj(times_, us_ali)
-    fig2, axes2 = aligator.utils.plotting.plot_velocity_traj(times_, vs_ali, rmodel)
-    _, axes3 = aligator.utils.plotting.plot_controls_traj(times_, us_altro, axes=axes1)
-    _, axes4 = aligator.utils.plotting.plot_velocity_traj(
-        times_, vs_altro, rmodel, axes=axes2
-    )
-    plt.show()
+
+    # iprunner = IpoptRunner({"max_iters": MAX_ITER})
+    # ipres = iprunner.solve(ur_problem, TOL)
+    # print("IpoptRunner result:", ipres)
+    # ipsolve = iprunner.solver
+
+    # xs_ipo = ipsolve.xs.tolist()
+    # us_ipo = ipsolve.us.tolist()
+    # vs_ipo = [x[nq:] for x in xs_ipo]
+
+    if args.plot:
+        fig1, axes1 = aligator.utils.plotting.plot_controls_traj(times_, us_ali)
+        fig2, axes2 = aligator.utils.plotting.plot_velocity_traj(times_, vs_ali, rmodel)
+        _, axes3 = aligator.utils.plotting.plot_controls_traj(
+            times_, us_altro, axes=axes1
+        )
+        _, axes4 = aligator.utils.plotting.plot_velocity_traj(
+            times_, vs_altro, rmodel, axes=axes2
+        )
+        plt.show()
