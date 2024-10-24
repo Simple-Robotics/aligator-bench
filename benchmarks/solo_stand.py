5d9ec54710495ca3d71edaaba3a9317c0f129ad6
ManifoldFR <wilson.jallet@polytechnique.org>
2024-10-25 00:52:49 +0200

    make solo_stand harder

--- benchmarks/solo_stand.py
+++ benchmarks/solo_stand.py
@@ -9,6 +9,7 @@ from aligator import (
     manifolds,
     underactuatedConstrainedInverseDynamics,
     ControlErrorResidual,
+    MultibodyFrictionConeResidual,
 )
 from aligator.utils import plotting
 from .common import load_solo12, Args
@@ -31,11 +32,18 @@ def get_default_pose():
 
 q_standing = get_default_pose()
 q_up_stand = q_standing.copy()
+torso_angle = np.deg2rad(-88)
+q_up_stand[4] = torso_angle
 q_up_stand[13] = 1.4
 q_up_stand[14] = 0.2
 q_up_stand[16] = 1.4
 q_up_stand[17] = 0.2
 hind_legs_idx = [13, 14, 16, 17]
+# front legs
+q_up_stand[7] = 1.7
+q_up_stand[8] = -0.6
+q_up_stand[10] = 1.7
+q_up_stand[11] = -0.6
 front_legs_idx = [7, 8, 10, 11]
 
 FOOT_NAMES = ("FL_FOOT", "FR_FOOT", "HL_FOOT", "HR_FOOT")
@@ -64,7 +72,7 @@ def get_constraint_models(feet_names, Kp, Kd=None):
             pl1,
             0,
             pl2,
-            pin.LOCAL_WORLD_ALIGNED,
+            pin.LOCAL,
         )
         rcm.corrector.Kp[:] = Kp
         rcm.corrector.Kd[:] = Kd
@@ -109,11 +117,9 @@ u0, _ = underactuatedConstrainedInverseDynamics(
     rmodel, rdata, q_standing, v0, act_matrix, rcms1, [cm.createData() for cm in rcms1]
 )
 
-t_stand = 0.6
+t_stand = 0.9
 t_up = 1.5
-xref_stand = xref.copy()
 xref_up_stand = np.concatenate((q_up_stand, v0))
-xref_stand[4] = np.deg2rad(-85)
 
 xs_init = []
 us_init = [np.zeros(nu)] * nsteps
@@ -123,27 +129,50 @@ for i in range(nsteps):
 
     Wx = np.ones(ndx)
     Wx[:nv] = 1e-3
+    Wx[:3] = 0.0
     _dyn = dyn1
     _target = xref.copy()
-    if ti >= t_stand:
+    rcost = aligator.CostStack(space, nu)
+    w_fr_w = 1e-2
+    if ti < t_stand:
+        for feet in FOOT_NAMES:
+            fr_cone = MultibodyFrictionConeResidual(
+                ndx, rmodel, act_matrix, rcms1, prox_settings, feet, 0.01
+            )
+            w_fr = w_fr_w * np.eye(fr_cone.nr)
+            rcost.addCost(
+                f"fric_{feet}",
+                aligator.QuadraticResidualCost(space, fr_cone, w_fr),
+                dt,
+            )
+    elif ti >= t_stand:
         # switch to standing
         _dyn = dyn2
-        _target[:] = xref_stand
-        Wx[:3] = 0.0
+        _target[:] = xref
+        _target[4] = torso_angle
         Wx[4] = 1.0
+        for feet in FOOT_NAMES_2:
+            fr_cone = MultibodyFrictionConeResidual(
+                ndx, rmodel, act_matrix, rcms2, prox_settings, feet, 0.01
+            )
+            w_fr = w_fr_w * np.eye(fr_cone.nr)
+            rcost.addCost(
+                f"fric_{feet}",
+                aligator.QuadraticResidualCost(space, fr_cone, w_fr),
+                dt,
+            )
     if ti >= t_up:
         Wx[hind_legs_idx] = 10.0
-        Wx[front_legs_idx] = 1e-4
+        Wx[front_legs_idx] = 1.0
         _target[:] = xref_up_stand
 
     xs_init.append(_target)
 
-    rcost = aligator.CostStack(space, nu)
     rcost.addCost(
         "xreg", aligator.QuadraticStateCost(space, nu, _target, np.diag(Wx)), dt
     )
     rcost.addCost(
-        "ureg", aligator.QuadraticControlCost(space, nu, np.eye(nu)), 1e-3 * dt
+        "ureg", aligator.QuadraticControlCost(space, nu, np.eye(nu)), 1e-4 * dt
     )
     stage = aligator.StageModel(rcost, _dyn)
     umax = rmodel.effortLimit
@@ -153,12 +182,12 @@ for i in range(nsteps):
     stages.append(stage)
 
 
-Wx_term = 1e-1 * np.ones(ndx)
+Wx_term = 1e-2 * np.ones(ndx)
 Wx_term[:3] = 0.0
 Wx_term[4] = 1.0
 Wx_term[hind_legs_idx] = 10.0
-Wx_term[front_legs_idx] = 1e-4
-xs_init.append(xref_stand)
+Wx_term[front_legs_idx] = 1.0
+xs_init.append(xref_up_stand)
 assert len(xs_init) == nsteps + 1
 assert len(stages) == nsteps
 term_cost = aligator.QuadraticStateCost(space, nu, xref_up_stand, np.diag(Wx_term))
@@ -177,6 +206,7 @@ if __name__ == "__main__":
             solver.setup(problem)
             solver.verbose = aligator.VERBOSE
             solver.rollout_type = aligator.ROLLOUT_LINEAR
+            solver.linesearch.avg_eta = 0.4
             solver.max_iters = MAX_ITER
             solver.run(problem, xs_init, us_init)
 
@@ -200,9 +230,7 @@ if __name__ == "__main__":
             raise ValueError("Solver not supported here")
     times = np.linspace(0, tf, nsteps + 1)
 
-    plotting.plot_controls_traj(
-        times, us_opt, effort_limit=rmodel.effortLimit, rmodel=rmodel
-    )
+    plotting.plot_controls_traj(times, us_opt, rmodel=rmodel)
     plt.show()
 
     viz = MeshcatVisualizer(
@@ -214,4 +242,4 @@ if __name__ == "__main__":
     input("[enter]")
 
     for i in range(4):
-        viz.play(qs_opt)
+        viz.play(qs_opt, dt=dt)
