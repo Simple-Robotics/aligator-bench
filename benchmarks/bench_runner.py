8be856cb4ab2a7fc6336a816436429fb28316a4c
ManifoldFR <wilson.jallet@polytechnique.org>
2024-10-25 17:30:03 +0200

    bench_runner: save run from bench runner itself, save solver config

--- benchmarks/bench_runner.py
+++ benchmarks/bench_runner.py
@@ -1,5 +1,6 @@
 import dataclasses
 import polars as pl
+import uuid
 
 from typing import Type, Tuple
 from .solver_runner import status_dtype
@@ -9,12 +10,25 @@ SolverConfig = Tuple[Type, dict]
 RESULTS_DIR = Path("results/")
 
 
+def _save_run(df: pl.DataFrame, bench_name):
+    df.write_csv(RESULTS_DIR / f"{bench_name}.csv")
+
+
 def run_benchmark_configs(
-    cls: Type, tol, instance_configs: list[dict], solver_configs: list[SolverConfig]
+    bench_name,
+    cls: Type,
+    tol,
+    instance_configs: list[dict],
+    solver_configs: list[SolverConfig],
 ):
+    import pickle
+
     data_ = []
+    suppl_data = {}
     for i, config in enumerate(instance_configs):
         example = cls(**config)
+        instance_name = example.name() + f"_{i}"
+        suppl_data["instance"][instance_name] = config.copy()
         for runner_cls, settings in solver_configs:
             runner = runner_cls(settings)
             entry = {"name": runner.name()}
@@ -24,16 +38,18 @@ def run_benchmark_configs(
             print(res)
             print("-------")
             entry.update(dataclasses.asdict(res))
-            instance_name = example.name() + f"_{i}"
+            run_id = uuid.uuid4()
+            entry["run_id"] = run_id.hex
             entry["instance"] = instance_name
+            entry["nsteps"] = example.problem.num_steps
             data_.append(entry)
+            suppl_data["solver"][run_id.hex] = settings.copy()
 
     df = pl.DataFrame(data_)
     df = df.rename({"status": "status_old"})
     df = df.with_columns(status=pl.col("status_old").cast(status_dtype))
     df.drop_in_place("status_old")
+    _save_run(df, bench_name)
+    with open(RESULTS_DIR / f"{bench_name}.pkl", "wb") as f:
+        pickle.dump(suppl_data, f)
     return df
-
-
-def save_run(df: pl.DataFrame, bench_name):
-    df.write_csv(RESULTS_DIR / f"{bench_name}.csv")
