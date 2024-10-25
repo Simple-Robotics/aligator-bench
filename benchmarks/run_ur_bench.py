31a1c16a96f010c7785eb22e4dc5f6e6812438cc
ManifoldFR <wilson.jallet@polytechnique.org>
2024-10-25 17:45:09 +0200

    fix run_ur_bench.py

--- benchmarks/run_ur_bench.py
+++ benchmarks/run_ur_bench.py
@@ -2,7 +2,7 @@ import numpy as np
 import polars
 from .ur5_problem import URProblem, generate_random_ee_target
 from .solver_runner import AltroRunner, ProxDdpRunner, IpoptRunner
-from .bench_runner import run_benchmark_configs, save_run
+from .bench_runner import run_benchmark_configs
 
 SEED = 42
 np.random.seed(SEED)
@@ -12,8 +12,6 @@ rmodel = URProblem.robot.model
 
 
 def run_with_vel(vel: bool, name):
-    import aligator
-
     num_instances = 10
     instance_configs = []
     for i in range(num_instances):
@@ -23,18 +21,23 @@ def run_with_vel(vel: bool, name):
     TOL = 1e-4
     MAX_ITERS = 400
 
-    mu_init = 1.0 if vel else 1.0
     default_start = True
     SOLVERS = [
         (AltroRunner, {"mu_init": 1.0, "tol_stationarity": 1e-4}),
         (
             ProxDdpRunner,
             {
-                "mu_init": mu_init,
+                "mu_init": 1.0,
                 "default_start": default_start,
-                "rollout_type": aligator.ROLLOUT_LINEAR
-                if vel
-                else aligator.ROLLOUT_NONLINEAR,
+                "rollout_type": "nonlinear",
+            },
+        ),
+        (
+            ProxDdpRunner,
+            {
+                "mu_init": 1e-1,
+                "default_start": default_start,
+                "rollout_type": "linear",
             },
         ),
         (IpoptRunner, {"print_level": 2, "default_start": default_start}),
@@ -45,6 +48,7 @@ def run_with_vel(vel: bool, name):
         settings["max_iters"] = MAX_ITERS
 
     df = run_benchmark_configs(
+        bench_name=name,
         cls=URProblem,
         tol=TOL,
         instance_configs=instance_configs,
@@ -54,8 +58,6 @@ def run_with_vel(vel: bool, name):
     with polars.Config(tbl_rows=-1, tbl_cols=10):
         print(df)
 
-    save_run(df, name)
 
-
-# run_with_vel(False, "ur5_reach")
-run_with_vel(True, "ur5_reach_vel")
+run_with_vel(False, "ur5_reach")
+# run_with_vel(True, "ur5_reach_vel")
