f62e42d84b56e937abbb526f9bac02c0e04c7f07
ManifoldFR <wilson.jallet@polytechnique.org>
2024-11-21 16:26:41 +0100

    Remove "robots" library, now moved to separate library: https://github.com/ManifoldFR/robot_descriptions_cpp
    + remove unfinished ur5.cpp bench

--- src/robots/instantiate.cpp
+++
@@ -1,9 +0,0 @@
-#include <pinocchio/parsers/urdf.hpp>
-
-namespace pinocchio {
-namespace urdf {
-template Model &
-buildModel<double, context::Options>(const std::string &filename, Model &model,
-                                     const bool verbose);
-} // namespace urdf
-} // namespace pinocchio
